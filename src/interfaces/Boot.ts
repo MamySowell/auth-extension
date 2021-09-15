22042ff53a8d512a3bfc95bf32941f0b4252e306
RAZAFIMANDIMBY Mamy Henintsoa <mamy@sowellapp.com>
2021-09-15 14:32:54 +0300

    extension initialisation
    app extension initialisation
    
    extension initialisation
    
    extension initialisation

--- src/interfaces/Boot.ts
+++ src/interfaces/Boot.ts
@@ -1,8 +1,8 @@
 import { Router } from "vue-router";
 import { Store } from "vuex";
-
+import { App } from "vue";
 export interface BootInterface {
   router: Router;
   store: Store<any>;
-  app: any;
+  app: App<any>;
 }
