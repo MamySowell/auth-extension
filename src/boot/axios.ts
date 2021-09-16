35c47d177ac02ee318cc14af075f72ec6549e31c
RAZAFIMANDIMBY Mamy Henintsoa <mamy@sowellapp.com>
2021-09-16 13:13:54 +0300

    error internationaization

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
 // FIXME: find another way to call this path
 //@ts-ignore
 import prompts from "app/quasar.extensions.json";
+import axios from "axios";
 import { boot } from "quasar/wrappers";
 import { BootInterface } from "../interfaces";
 
@@ -9,10 +9,9 @@ const {
   "@sowell/auth": { AUTH_SERVER_BASE_URL },
 } = prompts;
 
-const $api = axios.create({
-  baseURL: AUTH_SERVER_BASE_URL,
-});
-
 export default boot(({ app }: BootInterface) => {
+  const $api = axios.create({
+    baseURL: AUTH_SERVER_BASE_URL,
+  });
   app.provide("$api", $api);
 });
--- src/i18n/fr-FR/index.ts
+++ src/i18n/fr-FR/index.ts
@@ -10,5 +10,6 @@ export default {
     passwordPresenceError: "Le mot de passe doit être fourni",
     passwordLengthError: "La longueur du mot de passe doit être au minimum 6",
     loginFailed: "Identifiants incorrectes",
+    404: "Aucun utilisateur trouvé avec les identifiants donnés",
   },
 };
